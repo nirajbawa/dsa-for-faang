320c6b02f73f1fce932f0f104111e3182c8ac96c
Niraj bava <nirajbava222@gmail.com>
2025-12-15 16:25:02 +0530

    new code

---
+++ pattern/Inverted-Star-Pyramid.py
@@ -0,0 +1,8 @@
+for i in range(5):
+    for space in range(i):
+        print(" ", end="")
+    for star in range(5-i):
+        print("*", end="")
+    for star in range(5-i-1):
+        print("*", end="")
+    print("")
